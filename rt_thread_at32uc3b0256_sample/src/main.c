9044bdebb9ac6e6150ae3d9b9b0eb0f0f30730af
Raman Gopalan <ramangopalan@gmail.com>
2023-10-09 12:00:10 +0530

    Initial working commit: RT-Thread on UC3B: Needs more work

---
+++ rt_thread_at32uc3b0256_sample/src/main.c
@@ -0,0 +1,55 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * Bare minimum empty user application template
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# "Insert system clock initialization code here" comment
+ * -# Minimal main function that starts with a call to board_init()
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+#include <asf.h>
+
+//void delay (void)
+//{
+	//for (volatile int i = 0; i < 0xfff; i++)
+	//{
+		//
+	//}
+//}
+
+//int main (void)
+//{
+	///* Insert system clock initialization code here (sysclk_init()). */
+//
+	//board_init();
+	//gpio_configure_pin(AVR32_PIN_PA07, GPIO_DIR_OUTPUT | GPIO_INIT_HIGH);
+	//gpio_configure_pin(AVR32_PIN_PA08, GPIO_DIR_OUTPUT | GPIO_INIT_HIGH);
+	//while (1) {
+		//gpio_toggle_pin(AVR32_PIN_PA07);
+		//gpio_toggle_pin(AVR32_PIN_PA08);
+		//delay();
+	//}
+//
+	///* Insert application code here, after the board has been initialized. */
+//}
